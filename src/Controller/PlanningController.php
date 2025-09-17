a6c876d0248f5437a5d4632c4ae27fc565d7fcc9
DAUBREGE-Simon-2326036aa <simon.daubrege@etu.univ-amu.fr>
2025-09-17 09:18:16 +0200

    migration symfony branche simon

---
+++ src/Controller/PlanningController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PlanningController extends AbstractController
+{
+    #[Route('/planning', name: 'planning')]
+    public function index(): Response
+    {
+        return $this->render('planning/planning.html.twig');
+    }
+}
