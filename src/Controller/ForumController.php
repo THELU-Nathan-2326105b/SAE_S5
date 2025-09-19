eb3f9bc95af5f8c298490c17481878dc4d428a70
Daphne <aviasdaphne@gmail.com>
2025-09-19 12:06:27 +0200

    migration de la page forum, connexion a la bd sans entité, fix du css, etape 2 a réparer

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -5,12 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\DBAL\Connection;
 
-final class ForumController extends AbstractController
+class ForumController extends AbstractController
 {
     #[Route('/forum', name: 'forum')]
-    public function index(): Response
+    public function index(Connection $conn): Response
     {
-        return $this->render('forum/forum.html.twig');
+        $companies = $conn->fetchAllAssociative('SELECT company_name, company_description FROM company');
+
+        return $this->render('forum/forum.html.twig', [
+            'companies' => $companies,
+            'selected' => [],
+        ]);
     }
 }
