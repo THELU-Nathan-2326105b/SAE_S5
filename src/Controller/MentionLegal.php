a0f496a8a500ae497f756e02362b89b20fe46496
PELLET-Casimir-2326084 <casimir.pellet@etu.univ-amu.fr>
2025-09-19 11:54:39 +0200

    Création de la page mention légal

---
+++ src/Controller/MentionLegal.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MentionLegal extends AbstractController
+{
+    #[Route('/mentionlegal', name: 'mentionlegal')]
+    public function index(): Response
+    {
+        return $this->render('mentionlegal.html.twig');
+    }
+
+    #[Route('/forgot-password', name: 'forgot-password')]
+    public function forgotPassword(): Response
+    {
+        return $this->render('login/forgotpwd.html.twig');
+    }
+}
