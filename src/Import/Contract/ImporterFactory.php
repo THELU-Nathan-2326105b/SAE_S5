7e8f2e5a2683aa047e93b897c4b25a06270cf841
Darietto Nicolas <nicolas.darietto@etu.univ-amu.fr>
2025-11-13 01:00:55 +0100

    Ajout de parametre ContainerInterface  afin de localiser et retourner  une instance correspondant au parametre si existante

--- src/Import/Contract/ImporterFactory.php
+++ src/Import/Contract/ImporterFactory.php
@@ -6,21 +6,44 @@ namespace App\Import\Contract;
  */
 use InvalidArgumentException;
 use Psr\Container\ContainerInterface;
+use App\Import\Contract\Importer; 
 
 class ImporterFactory
 {
     private ContainerInterface $locator;
 
 
-    public function __construct() {}
+    public function __construct(ContainerInterface $locator) {
+        $this->locator=$locator;
+    }
 
 
     public function create(string $entity, string $format = 'csv'): Importer
     {
-        $key = strtolower("$format.$entity"); 
-        if (!$this->locator->has($key)){
-            throw new \InvalidArgumentException("Pas d'importeur pour $key");
+        $entity = strtolower(trim($entity));
+        $format = strtolower(trim($format));
+
+        if ($entity==='' || $format==='') {
+            throw new InvalidArgumentException('format et entity sont obligatoires.');
         }
-        return $this->locator->get($key); 
+        else{
+            $key = "{$format}.{$entity}";
+            if (!$this->locator->has($key)) {
+                throw new InvalidArgumentException("Pas d'importeur pour {$key}");
+            }
+            else{
+                $importer = $this->locator->get($key);
+                if (!$importer instanceof Importer) {
+                    throw new InvalidArgumentException("Le service '{$key}' n'implémente pas ".Importer::class);
+                }
+                else{
+                    return $importer;
+                }
+
+                
+                
+            }
+            
+        }   
     }
 }
