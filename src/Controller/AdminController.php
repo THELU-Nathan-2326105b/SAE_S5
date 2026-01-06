52b6a2ff201eabfff3518ac2c44c9f08ab68ad9c
DAUBREGE-Simon-2326036aa <simon.daubrege@etu.univ-amu.fr>
2026-01-06 15:04:42 +0100

    correction navbar avec menu burger

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -34,7 +34,7 @@ final class AdminController extends AbstractController
         ]);
     }
 
-    #[Route('/admin/forum', name: 'admin_companies')]
+    #[Route('/admin/forum', name: 'admin_forum')]
     public function showForum(EntityManagerInterface $em): Response
     {
         return $this->render('admin/forum.html.twig');
