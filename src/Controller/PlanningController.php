a9b3f83fad07b5154278cf78e133b5ed0d45a797
CURT-Elien-2326035a <eliencurt13@gmail.com>
2026-01-07 05:05:38 +0100

    fix

--- src/Controller/PlanningController.php
+++ src/Controller/PlanningController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ForumRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class PlanningController extends AbstractController
 {
     #[Route('/planning', name: 'planning')]
-    public function index(): Response
+    public function index(ForumRepository $forumRepository): Response
     {
-        return $this->render('planning/planning.html.twig');
+        $forums = $forumRepository->findAll();
+        
+        return $this->render('planning/planningalgo.html.twig', [
+            'forums' => $forums,
+        ]);
     }
 }
